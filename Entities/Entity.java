8b7fafdbc68f223a3313076c6a280c63d348608c
Andrea-004 <00106123@uca.edu.sv>
2024-05-30 18:46:58 -0600

    Adding entities

---
+++ Entities/Entity.java
@@ -0,0 +1,54 @@
+package Entities;
+import PokemonFactory.Pokemon.Pokemon;
+import java.util.ArrayList;
+
+
+public class Entity {
+    private String name;
+    private ArrayList<Pokemon> pokemons;
+    private int Xposition;
+    private int YPosition;
+
+    public Entity() {
+    }
+
+    public Entity(String name, ArrayList<Pokemon> pokemons, int xposition, int YPosition) {
+        this.name = name;
+        this.pokemons = pokemons;
+        Xposition = xposition;
+        this.YPosition = YPosition;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Pokemon> getPokemons() {
+        return pokemons;
+    }
+
+    public void setPokemons(ArrayList<Pokemon> pokemons) {
+        this.pokemons = pokemons;
+    }
+
+    public int getXposition() {
+        return Xposition;
+    }
+
+    public void setXposition(int xposition) {
+        Xposition = xposition;
+    }
+
+    public int getYPosition() {
+        return YPosition;
+    }
+
+    public void setYPosition(int YPosition) {
+        this.YPosition = YPosition;
+    }
+
+}
---
+++ Entities/Player.java
@@ -0,0 +1,41 @@
+package Entities;
+
+import Items.Buyable;
+import PokemonFactory.Pokemon.Pokemon;
+
+
+import java.util.ArrayList;
+
+public class Player extends Entity {
+    private ArrayList<Buyable> items = new ArrayList<>();
+    private int money;
+
+    public Player() {
+
+    }
+
+    public Player(String name, ArrayList<Pokemon> pokemons, int xposition, int YPosition, ArrayList<Buyable> items, int money) {
+        super(name, pokemons, xposition, YPosition);
+        this.items = items;
+        this.money = money;
+    }
+
+    public ArrayList<Buyable> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Buyable> items) {
+        this.items = items;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void move(){}
+
+}
